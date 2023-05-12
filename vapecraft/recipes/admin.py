33f57d8fd033379769030f1a89d6fafb5d057139
Денис (home) <dultsevdv@gmail.com>
2023-05-12 18:52:23 +0500

    Настройка рецептов в адимнке

--- vapecraft/recipes/admin.py
+++ vapecraft/recipes/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from . import models
 
+class RecipesAdmin(admin.ModelAdmin):
+    list_display = ('name', 'rating' , 'created', 'modified')
+    list_display_links = ('name',)
+    list_filter = ('name',)
 
-admin.site.register(models.Recipe)
+admin.site.register(models.Recipe, RecipesAdmin)
