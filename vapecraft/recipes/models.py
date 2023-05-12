fe1d73245fd7155e5b6d84999871ffdc78e70216
Денис (home) <dultsevdv@gmail.com>
2023-05-12 18:09:29 +0500

    Модель рецепта

--- vapecraft/recipes/models.py
+++ vapecraft/recipes/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Recipe(models.Model):
+    name = models.CharField("Название", max_length=50, unique=True)
+    description = models.TextField("Описание", blank=True, null=True)
+    rating = models.IntegerField('Рейтинг')
+    image = models.ImageField('Изображение', upload_to='img/recipes/', height_field=None, width_field=None, max_length=None)
+
+    def __str__(self) -> str:
+        return self.name
