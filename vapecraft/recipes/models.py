ff62d263ca0e44727fb020278c8e8ec672531902
Денис (home) <dultsevdv@gmail.com>
2023-05-12 22:38:54 +0500

    get_absolute_url

--- vapecraft/recipes/models.py
+++ vapecraft/recipes/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
+from django.urls import reverse
 
 class Recipe(models.Model):
     name = models.CharField("Название", max_length=50, unique=True)
@@ -11,6 +12,11 @@ class Recipe(models.Model):
 
     def __str__(self) -> str:
         return self.name
+
+
+    def get_absolute_url(self):
+        return reverse("recipes", kwargs={})
+    
 
 
     class Meta:
