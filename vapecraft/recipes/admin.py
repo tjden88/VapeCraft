2fb33343a97b715ca9481e2f9a18035c29a2939e
Денис (home) <dultsevdv@gmail.com>
2023-05-17 02:43:44 +0500

    Правки

--- vapecraft/recipes/admin.py
+++ vapecraft/recipes/admin.py
@@ -1,9 +1,11 @@
+from django import forms
 from django.contrib import admin
 from . import models
 
 class RecipesAdmin(admin.ModelAdmin):
     list_display = ('name', 'rating' , 'created', 'modified')
     list_display_links = ('name',)
-    list_filter = ('name',)
-
+    search_fields = ('name',)
+        
+        
 admin.site.register(models.Recipe, RecipesAdmin)
--- vapecraft/recipes/models.py
+++ vapecraft/recipes/models.py
@@ -16,7 +16,7 @@ def get_file_path(instance, filename):
 class Recipe(models.Model):
     name = models.CharField("Название", max_length=50, unique=True)
     description = models.TextField("Описание", blank=True, null=True)
-    rating = models.IntegerField('Рейтинг', validators=[MinValueValidator(0), MaxValueValidator(5)])
+    rating = models.IntegerField('Рейтинг', validators=[MinValueValidator(0), MaxValueValidator(5)], default=0)
     image = models.ImageField('Изображение', upload_to=get_file_path, height_field=None, width_field=None, max_length=None, blank=True)
     created = models.DateTimeField("Создан", auto_now_add=True)
     modified = models.DateTimeField("Обновлён", auto_now=True)
